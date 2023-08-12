91b529bd62bcdbbb8379a1f1fd13726ac59645d2
Mahdi Tasha <imwhdiiii@gmail.com>
2023-08-12 15:09:34 +0330

    created routes of pages

---
+++ app/monthly/medications/page.tsx
@@ -0,0 +1,10 @@
+// Codes By Mahdi Tasha
+// Creating Monthly Medications Page And Exporting It As Default
+export default function MonthlyMedications():JSX.Element {
+    // Returning JSX
+    return (
+        <div>
+            <h1>Monthly Medications</h1>
+        </div>
+    );
+}
---
+++ app/monthly/meditations/page.tsx
@@ -0,0 +1,10 @@
+// Codes By Mahdi Tasha
+// Creating Monthly Meditations Page And Exporting It As Default
+export default function MonthlyMeditations():JSX.Element {
+    // Returning JSX
+    return (
+        <div>
+            <h1>Monthly Meditations</h1>
+        </div>
+    );
+}
