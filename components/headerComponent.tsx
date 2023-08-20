52e226ce33426fa92961ed8584d8ba9628394d64
Mahdi Tasha <imwhdiiii@gmail.com>
2023-08-20 19:02:45 +0330

    removed login page

--- components/headerComponent.tsx
+++ components/headerComponent.tsx
@@ -11,7 +11,7 @@ export default function HeaderComponent():JSX.Element {
         <header className={'bg-headerBg'}>
             <HolderComponent className={'flex gap-3 items-center justify-between py-[10px]'}>
                 <Link href={'/'} className={'tracking-widest text-black truncate'}>I WILL BE GOOD</Link>
-                <Link href={'/login'}><img className={'w-[50px] h-[50px] aspect-square object-cover rounded-full'} src={ProfileImage.src} alt="Profile Image"/></Link>
+                <img className={'w-[50px] h-[50px] aspect-square object-cover rounded-full'} src={ProfileImage.src} alt="Profile Image"/>
             </HolderComponent>
         </header>
     );
