1914b798f1d99d1a93ae7470ab882031268807a2
Mahdi Tasha <imwhdiiii@gmail.com>
2023-10-12 13:10:34 +0330

    added sertraline monthly reports page

--- app/medication/sertraline/page.tsx
+++ app/medication/sertraline/page.tsx
@@ -1,11 +1,26 @@
 // Codes By Mahdi Tasha
 // Importing Part
 import {ReactNode} from "react";
+import CalenderComponent from "@/component/calender/calenderComponent";
+import ThirdSectionComponent from "@/component/section/home/thirdSectionComponent";
+import HolderComponent from "@/chunk/holderComponent";
 
 // Creating And Exporting Sertraline Page As Default
 export default function SertralinePage():ReactNode {
     // Returning JSX
     return (
-        <h1>Sertraline Page</h1>
+        <HolderComponent>
+            <section>
+                <main>
+                    <h2 className={'title'}>Monthly report of sertraline:</h2>
+                    <div className={'grid lg:grid-cols-2 lg:gap-[20px] gap-[10px]'}>
+                        <CalenderComponent doneDaysArray={[1,2,3]} />
+                        <p className={'paragraph'}>is an antidepressant of the selective serotonin reuptake inhibitor (SSRI) class.The efficacy of sertraline for depression is similar to that of other antidepressants, and the differences are mostly confined to side effects.</p>
+                    </div>
+                </main>
+            </section>
+            <hr className={'section-divider'} />
+            <ThirdSectionComponent />
+        </HolderComponent>
     );
 }
