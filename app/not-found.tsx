95927fd6a42c6db0c2bbca4b9042918353463c73
Mahdi Tasha <imwhdiiii@gmail.com>
2023-10-12 11:20:45 +0330

    added structure, style and mqs of  not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,24 @@
 // Codes By Mahdi Tasha
 // Importing Part
 import {ReactNode} from "react";
+import Image from '@/public/assets/img/img-404.png';
+import HolderComponent from "@/chunk/holderComponent";
 
 // Creating And Exporting 404 Page As Default
 export default function NotFoundPage():ReactNode {
     // Returning JSX
     return (
-        <h1>Not Found Page</h1>
+        <HolderComponent>
+            <section>
+                <main>
+                    <img className={'lg:w-[70%] w-full mx-auto mb-[30px]'} src={Image.src} alt="Illustration"/>
+                    <h1 className={'text-center'}>
+                        <span className={'lg:text-[24px] text-[20px] font-normal'}>The page your looking for is</span>
+                        <br/>
+                        <span className={'lg:text-[32px] text-[25px] font-bold'}>Not Found</span>
+                    </h1>
+                </main>
+            </section>
+        </HolderComponent>
     );
 }
