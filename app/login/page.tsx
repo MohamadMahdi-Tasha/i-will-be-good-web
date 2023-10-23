f772b1a177610b908c11df36fb9f49880ad263ea
Mahdi Tasha <imwhdiiii@gmail.com>
2023-10-23 17:02:07 +0330

    added structure of new login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,11 +2,18 @@
 // Importing Part
 import {ReactNode} from "react";
 import HolderComponent from "@/chunk/holderComponent";
+import InputComponent from "@/chunk/inputComponent";
 
 // Creating And Exporting Login Page As Default
 export default function LoginPage():ReactNode {
     // Returning JSX
     return (
-        <HolderComponent>asdasdsa</HolderComponent>
+        <HolderComponent>
+            <form action="#">
+                <InputComponent type={'email'} required id={'email-input'} placeHolder={'Email:'} />
+                <InputComponent type={'password'} required id={'password-input'} placeHolder={'Password:'} />
+                <button>SUBMIT</button>
+            </form>
+        </HolderComponent>
     );
 }
